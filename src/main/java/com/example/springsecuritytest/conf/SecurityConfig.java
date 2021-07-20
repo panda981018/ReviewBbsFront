6ab8b61dd471434b18affa947cbfc214454f251b
jw.chung <jw.chung@konai.com>
2021-07-20 09:12:26 +0900

    change some remarks

--- src/main/java/com/example/springsecuritytest/conf/SecurityConfig.java
+++ src/main/java/com/example/springsecuritytest/conf/SecurityConfig.java
@@ -26,13 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    // WebSecurity는 FilterChainProxy를 생성하는 필터.
-    public void configure(WebSecurity web) throws Exception {
-        // 파일 기준은 resources/static 디렉토리.
-        web.ignoring().antMatchers("classpath:/resources/templates");
-    }
-
-    @Override
     // HttpStatus를 통해 HTTP 요청에 대한 웹 기반 보안을 구성할 수 있다.
     protected void configure(HttpSecurity http) throws Exception {
         // authorizeRequests() : HttpServletRequest에 따라 접근을 제한함.
@@ -58,7 +51,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    // 모든 인증은 AuthenticationManager를 통해 이루어지며, AuthenticationManager를 생성하기 위해서는 Builder를 사용해야함.
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
--- src/main/java/com/example/springsecuritytest/domain/repository/MemberRepository.java
+++ src/main/java/com/example/springsecuritytest/domain/repository/MemberRepository.java
@@ -10,4 +10,7 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     // email을 where 절로 하여, 데이터를 가져올 수 있도록 findByEmail() 메서드를 정의함.
     Optional<MemberEntity> findByEmail(String userEmail);
 
+    // email 중복 여부 확인용
+    boolean existsByEmail(String userEmail);
+
 }
