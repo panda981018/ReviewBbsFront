1372f6c914053ed0c37704cb65b4353582788db1
jw.chung <jw.chung@konai.com>
2021-10-06 08:35:18 +0900

    tui pagination(client pagination)

--- src/main/resources/static/js/postGrid.js
+++ src/main/resources/static/js/postGrid.js
@@ -1,7 +1,6 @@
 let bbsCategoryId = 0; // category 아이디(1번부터~)
-let grid; // grid 객체
 let pagination; // grid의 pagination 객체
-let bbsData; // bbs에 저장된 객체 집합
+let grid;
 
 function getCategoryId(categoryId) {
     bbsCategoryId = categoryId;
@@ -10,9 +9,13 @@ function getCategoryId(categoryId) {
 function createGrid() {
     grid = new tui.Grid({
         el: $('#grid')[0],
+        minBodyHeight: 40,
         scrollX: false,
-        pageOptions: { perPage : 10 },
         scrollY: false,
+        pageOptions: {
+            useClient: true,
+            perPage: 10
+        },
         columns: [
             {
                 header: '번호',
@@ -24,97 +27,60 @@ function createGrid() {
             },
             {
                 header: '제목',
-                name: 'title',
+                name: 'bbsTitle',
                 resizable: true,
-                sortable: true,
-                sortingType: 'desc',
                 align: "left"
             },
             {
                 header: '작성자',
-                name: 'writer',
+                name: 'bbsWriter',
                 width: 65,
-                sortable: true,
-                sortingType: 'desc',
                 align: "center"
             },
             {
                 header: '작성날짜',
-                name: 'createDate',
+                name: 'bbsDate',
                 width: 135,
                 resizable: true,
-                sortable: true,
-                sortingType: 'desc',
                 align: "center"
             },
             {
                 header: '조회수',
-                name: 'views',
+                name: 'bbsViews',
                 width: "auto",
-                sortable: true,
-                sortingType: 'desc',
                 align: "center"
             },
             {
                 header: '댓글수',
-                name: 'replies',
+                name: 'replyCnt',
                 width: "auto",
-                sortable: true,
-                sortingType: 'desc',
                 align: "center"
             }
-        ]
-    });
-}
-
-function createPagination() {
-    pagination = grid.getPagination();
-    pagination.setTotalItems(bbsData.length);
-    pagination.centerAlign = true;
-    // pagination = new tui.Pagination($('#pagination'), {
-    //     totalItems: bbsData.length,
-    //     itemsPerPage: 10,
-    //     visiblePages: 5,
-    //     centerAlign: true
-    // });
+        ],
+        useClientSort: true
+    }); // grid 객체
 }
 
 function callBbsData() {
     console.log('url: /api/bbs/get?category=' + bbsCategoryId);
+
     $.ajax({
         method: 'GET',
         url: '/api/bbs/get?category=' + bbsCategoryId,
         dataType: 'json',
-        success: function (bbsList) {
-            if (bbsList.bbsList.length !== 0) {
-                bbsData = bbsList.bbsList;
+        success: function (jsonMap) {
+            if (jsonMap.data.length !== 0) {
                 createGrid();
-                createPagination();
                 $('#tableInfo').addClass("mb-3 d-flex flex-row justify-content-start align-items-center")
                     .css('display', 'block');
                 $('#noData').css('display', 'none');
                 $('#grid').css('display', 'block');
-                setGridData(bbsList.bbsList);
-            } else if (bbsList.bbsList.length == 0) { // 데이터가 없을 때
+                grid.resetData(jsonMap.data);
+                // setGridData(jsonMap.data);
+            } else { // 데이터가 없을 때
                 $('#tableInfo').css('display', 'none');
                 $('#noData').css('display', 'block');
             }
         }
     })
-}
-
-function setGridData(bbsList) {
-    let data = [];
-    for (let i = 0; i < bbsList.length; i++) {
-        const bbs = {};
-        bbs.id = bbsList[i].id;
-        bbs.title = bbsList[i].bbsTitle;
-        bbs.writer = bbsList[i].bbsWriter;
-        bbs.createDate = bbsList[i].bbsDate;
-        bbs.views = bbsList[i].bbsViews;
-        bbs.replies = bbsList[i].replyCnt;
-
-        data.push(bbs);
-    }
-    grid.resetData(data);
 }
